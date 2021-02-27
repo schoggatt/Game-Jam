c4106797a1ac8d8a89fcae3391bdb57ea362aab9
schoggatt <samuelchoggatt@gmail.com>
2021-02-27 11:39:30 -0600

    setup

---
+++ Assets/Forest/Parallax.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Adds Parallax effect
+/// </summary>
+public class Parallax : MonoBehaviour
+{
+    public Camera cam;
+    public Transform subject;
+
+    Vector2 startPosition;
+    float startZ;
+
+
+    Vector2 travel => (Vector2)cam.transform.position - startPosition; // distance moved from camera to original sprite
+    Vector2 parallaxVector;
+
+    float distanceFromSubject => transform.position.z - subject.position.z;
+    float clippingPlane => cam.transform.position.z + (distanceFromSubject > 0 ? cam.farClipPlane : cam.nearClipPlane);
+
+    float parallaxFactor => Mathf.Abs(distanceFromSubject) / clippingPlane;
+
+    public void Start()
+    {
+        startPosition = transform.position;
+        startZ = transform.position.z;
+    }
+
+    public void Update()
+    {
+        Vector2 newPos = transform.position = startPosition + travel * parallaxFactor;
+        transform.position = new Vector3(newPos.x, newPos.y, startZ);
+    }
+}
---
+++ Assets/Main Guy/Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Handles Player Movement
+/// </summary>
+public class Movement : MonoBehaviour
+{
+    #region References
+    public CharacterController2D controller;
+    public Transform CamTransform;
+    public Transform PlayerTransform;
+    public Animator animator;
+    #endregion
+    #region Fields
+    bool jump;
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    #endregion
+
+    private void Start()
+    {
+        CamTransform = Camera.main.transform; // Attach cam
+    }
+
+    void Update()
+    {
+        var direction = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("RunningSpeed", direction);
+        animator.SetFloat("IsRunning", Mathf.Abs(direction));
+        horizontalMove = direction * runSpeed;
+    }
+
+    /// <summary>
+    /// Called every fixed frame rate
+    /// </summary>
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        CamTransform.position = new Vector3(PlayerTransform.position.x, CamTransform.position.y, CamTransform.position.z);
+
+
+        jump = false;
+    }
+}
